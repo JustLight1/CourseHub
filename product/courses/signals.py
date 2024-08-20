787e2d9865f02197710d2f44361f02111bfe805b
Форов Александр <5h9t5@list.ru>
2024-08-21 00:21:56 +0300

    feat(signals): update post_save_subscription

--- product/courses/signals.py
+++ product/courses/signals.py
@@ -1,8 +1,10 @@
 from django.db.models import Count
 from django.db.models.signals import post_save
+from django.db import transaction
 from django.dispatch import receiver
-from django.utils import timezone
+
 from users.models import Subscription
+from courses.models import Group
 
 
 @receiver(post_save, sender=Subscription)
@@ -12,6 +14,24 @@ def post_save_subscription(sender, instance: Subscription, created, **kwargs):
 
     """
 
-    if created:
-        pass
-        # TODO
+    if created and instance.is_active:
+        user = instance.user
+        course = instance.course
+        groups = Group.objects.filter(course=course)
+        if not groups.exists():
+            return
+        sorted_groups = groups.annotate(
+            student_count=Count('students')).order_by('student_count')
+        suitable_group = None
+        for group in sorted_groups:
+            if group.students.count() < group.max_students:
+                suitable_group = group
+                break
+
+        if suitable_group is None and sorted_groups:
+            suitable_group = sorted_groups[0]
+
+        if suitable_group:
+            with transaction.atomic():
+                suitable_group.students.add(user)
+                suitable_group.save()
